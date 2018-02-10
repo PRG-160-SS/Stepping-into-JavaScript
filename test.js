e82233004b56dcf88151ae8d04f0b73e02fc49c8
[savannahswartzel] <[ss55484@ravens.racc.edu]>
2018-02-10 14:16:51 -0500

    modified test.js with code challenge string, differences, average

--- test.js
+++ test.js
@@ -1,9 +1,40 @@
-function getAverage (a,b,c,d,e,f){
+function getAverage (a,b,c,d,e){
 	
-	var average = (a + b + c + d + e + f) / 6;
+	var average = (a + b + c + d + e) / 5;
 	return(average);
 	
 }
 
-var myResult = getAverage(7,8,9,10,11,12);
+var myResult = getAverage(7,8,9,10,11);
 console.log("The average is " + myResult);
+
+
+function getDifference (m,n){
+	
+	var difference = (m - n);
+	return(difference);
+	
+}
+
+var diffResults = getDifference(25,10);
+console.log("The difference is " + diffResults);
+
+
+function myName (t,s) {
+
+	var name = (t + s);
+	return(name);
+
+}
+
+var nameResult = myName('Savannah',' Swartzel');
+console.log("My name is " + nameResult);
+
+
+
+
+
+
+
+
+
